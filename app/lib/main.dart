90c7fa385714adf5eb88e01360786c7e36dfd1f6
Mooobert <misha@burnayev.com>
2023-05-08 17:57:21 -0400

    Cloud Storage added, Realtime Database removed

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
