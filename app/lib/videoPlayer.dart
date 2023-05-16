ad612ddce41049421143fc4915dd77fb2b7bd8d0
Mooobert <misha@burnayev.com>
2023-05-15 23:26:15 -0400

    videoPlayer foundation added

--- app/lib/videoPlayer.dart
+++ app/lib/videoPlayer.dart
@@ -2,3 +2,21 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:video_player/video_player.dart';
 
+class Video extends StatefulWidget {
+  const Video({super.key});
+
+  @override
+  State<Video> createState() => _VideoState();
+}
+
+class _VideoState extends State<Video> {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: singleVideo);
+  }
+
+  Widget get singleVideo {
+    return const Scaffold();
+  }
+}
